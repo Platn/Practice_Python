8722d1c01abae9535534cc12c8334fa60582c718
Kevin Le <platn02@gmail.com>
2017-08-01 18:39:33 -0700

    Palindrome, Reverse String List Check
    http://www.practicepython.org/exercise/2014/03/12/06-string-lists.html

--- Python_Programs/05_List_Overlap.py
+++ Python_Programs/05_List_Overlap.py
@@ -7,6 +7,7 @@ b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 # a,b = [random.sample(range(30), 4), random.sample(range(30), 4)]
 c = []
 # For each number of b, check if it is in a and not already in c
+
 for numB in b:
     if(numB in a and not(numB in c)):
         c.append(numB)
---
+++ Python_Programs/06_String_Lists.py
@@ -0,0 +1,4 @@
+inputString = input("Please enter some text: ")
+
+if(inputString == inputString[::-1]):
+    print("Your input is a palindrome.")
